5c1fc607864308c33498ab751bb51619c158b294
SOHO Admin <info@soho.fitness>
2025-08-19 17:35:59 +0000

    okay nice, we are going somewhere after u limited the scroll and now the

--- src/components/document-list.tsx
+++ src/components/document-list.tsx
@@ -20,7 +20,7 @@ export function DocumentList({ documents }: DocumentListProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {documents.map((doc) => (
         <DocumentCard key={doc.id} document={doc} />
       ))}

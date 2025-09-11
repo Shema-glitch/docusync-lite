9a359d68a916baf43d7b921604c5ef0fb6186924
Shema Charmant <charmantshema112@gmail.com>
2025-09-11 22:37:04 +0000

    heres the code "const firebaseConfig = {   apiKey: "AIzaSyDQPWIV5GJorq0s

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 // It looks like you've provided some code to start. We'll be using this as a reference to get you connected to Firebase.
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDQPWIV5GJorq0sb9JZ-2hpTC9YEtwEP8A",
+  authDomain: "docusync-lite-4254f.firebaseapp.com",
+  projectId: "docusync-lite-4254f",
+  storageBucket: "docusync-lite-4254f.appspot.com",
+  messagingSenderId: "146120250858",
+  appId: "1:146120250858:web:f2cb3647644048b80f4e93"
 };
 
 // Initialize Firebase

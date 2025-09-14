7dc01c62bd2a41a323c3dfa367555b601d3d4b83
Shema Charmant <charmantshema112@gmail.com>
2025-09-14 01:09:19 +0000

    A param property was accessed directly with `params.id`. `params` is now

--- src/app/(main)/profile/[id]/page.tsx
+++ src/app/(main)/profile/[id]/page.tsx
@@ -10,8 +10,7 @@ import { Loader2, AlertTriangle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 
-export default function ProfilePage({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default function ProfilePage({ params: { id } }: { params: { id: string } }) {
   const { findUserById } = useDocuments();
   const [profileUser, setProfileUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);

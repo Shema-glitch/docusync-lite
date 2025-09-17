ac5c752342efc2629365e20f6c122841546197dc
SOHO Admin <info@soho.fitness>
2025-09-17 11:15:43 +0000

    can i have the logo of the google and microsoft like colored not black a

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,20 +13,22 @@ import { ForgotPasswordDialog } from '@/components/auth/forgot-password-dialog';
 import { Alert, AlertDescription } from '@/components/ui/alert';
 
 const GoogleIcon = () => (
-    <svg className="h-5 w-5" viewBox="0 0 24 24">
-        <path
-            fill="currentColor"
-            d="M21.35,11.1H12.18V13.83H18.69C18.36,17.64 15.19,19.27 12.19,19.27C8.36,19.27 5,16.25 5,12C5,7.9 8.2,4.73 12.19,4.73C14.03,4.73 15.69,5.36 16.95,6.58L19.35,4.36C17.27,2.46 15,1.5 12.19,1.5C6.92,1.5 3,6.08 3,12C3,17.92 6.92,22.5 12.19,22.5C17.6,22.5 21.7,18.43 21.7,12.33C21.7,11.77 21.5,11.45 21.35,11.1Z"
-        />
+    <svg className="h-5 w-5" viewBox="0 0 48 48">
+        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+        <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+        <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+        <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+        <path fill="none" d="M0 0h48v48H0z"></path>
     </svg>
 );
 
 const MicrosoftIcon = () => (
-    <svg className="h-5 w-5" viewBox="0 0 24 24">
-        <path 
-            fill="currentColor"
-            d="M11.5,3.5H3.5v8h8Zm9,0h-8v8h8Zm-9,9H3.5v8h8Zm9,0h-8v8h8Z" 
-        />
+    <svg className="h-5 w-5" viewBox="0 0 23 23">
+        <path fill="#f3f3f3" d="M0 0H23V23H0z" />
+        <path fill="#F25022" d="M1 1h10v10H1z" />
+        <path fill="#00A4EF" d="M1 12h10v10H1z" />
+        <path fill="#7FBA00" d="M12 1h10v10H12z" />
+        <path fill="#FFB900" d="M12 12h10v10H12z" />
     </svg>
 );
 
